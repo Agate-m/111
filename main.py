96a938f8d1bf6ef8a71a674e1caa76f6bd99ea73
Agate-m <1738568876@qq.com>
2026-09-13 20:17:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ SLEEPTIME = 1  # 每次抢座的间隔
 ENDTIME = "20:02:00"  # 根据学校的预约座位时间+1min即可
 
 ENABLE_SLIDER = True  # 是否有滑块验证
-MAX_ATTEMPT = 3  # 最大尝试次数
+MAX_ATTEMPT = 5  # 最大尝试次数
 RESERVE_NEXT_DAY = False  # 预约明天而不是今天的
 
 
